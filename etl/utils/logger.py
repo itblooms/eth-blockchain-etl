8113701f65fca64cdd7991841d424b36fe722698
Said Osmanov <saidelleausmaneuff@gmail.com>
2026-04-08 00:38:07 +0300

    Change standard Logger to PySparkLogger

--- etl/utils/logger.py
+++ etl/utils/logger.py
@@ -1,19 +1,8 @@
+from pyspark.logger import PySparkLogger
 import logging
 
 
-def get_logger(name: str) -> logging.Logger:
-    logger = logging.getLogger(name)
+def get_logger(name: str) -> PySparkLogger:
+    logger = PySparkLogger.getLogger(name)
     logger.setLevel(logging.DEBUG)
-
-    formatter = logging.Formatter(
-        fmt="{asctime} - {levelname}:{name}: {message}",
-        style="{",
-        datefmt="%Y-%m-%d %H:%M"
-    )
-
-    console_handler = logging.StreamHandler()  # EMR captures stdout → S3
-    console_handler.setLevel(logging.DEBUG)
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
-
     return logger
