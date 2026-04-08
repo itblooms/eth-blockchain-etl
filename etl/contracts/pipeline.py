ba72b08902412a5032121d2aeb442e9018c20d04
Said Osmanov <saidelleausmaneuff@gmail.com>
2026-04-08 23:30:37 +0300

    Add data cleaning func

--- etl/contracts/pipeline.py
+++ etl/contracts/pipeline.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession, DataFrame
 import pyspark.sql.functions as F
+from pyspark.sql.types import StringType, StructField, StructType
 from etl.utils.logger import get_logger
 
 
@@ -7,12 +8,11 @@ logger = get_logger(__name__)
 
 def extract_contracts_data(spark: SparkSession, s3_bucket_url: str) -> DataFrame:
     try:
-        logger.info(f"Fetching transactions data from {s3_bucket_url}/contracts")
-        contracts_df = spark.read.parquet(f"{s3_bucket_url}/contracts/*", header=True) \
+        logger.info(f"Fetching contracts data from {s3_bucket_url}/contracts")
+        contracts_df = spark.read.parquet(f"{s3_bucket_url}/contracts/date=2026-*-*", header=True) \
             .select(
                 F.col("address"),
-                F.col("bytecode"),
-                F.col("block_number")
+                F.col("bytecode")
             )
     except FileNotFoundError:
         logger.error(f"Fetching failed. There's no files at {s3_bucket_url}/contracts/")
@@ -21,3 +21,16 @@ def extract_contracts_data(spark: SparkSession, s3_bucket_url: str) -> DataFrame
         logger.error(f"Contracts data extraction failed with unexpected error: {e}")
         raise
     return contracts_df
+
+def clean_contracts_data(df: DataFrame) -> DataFrame:
+    logger.info(f"Casting column types and trimming stings...")
+    transformed_df = (
+        df
+        .drop_duplicates()
+        .select(
+            F.trim(F.col("address").cast(StringType())),
+            F.trim(F.col("bytecode").cast(StringType()))
+        )
+    )
+    logger.info("Contracts data was successfully cleaned!")
+    return transformed_df
