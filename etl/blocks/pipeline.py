500a5ac6bab5cfa7385ee8170915f9f08bc15a03
Said Osmanov <saidelleausmaneuff@gmail.com>
2026-04-08 22:43:14 +0300

    Add data cleaning func

--- etl/blocks/pipeline.py
+++ etl/blocks/pipeline.py
@@ -1,6 +1,14 @@
 from pyspark.sql import SparkSession, DataFrame
 import pyspark.sql.functions as F
-from pyspark.sql.types import IntegerType, FloatType, StringType, DateType
+from pyspark.sql.types import (
+    LongType, 
+    DoubleType, 
+    StringType, 
+    TimestampType, 
+    DateType, 
+    StructField,
+    StructType
+)
 from etl.utils.logger import get_logger
 
 
@@ -8,11 +16,13 @@ logger = get_logger(__name__)
 
 def extract_blocks_data(spark: SparkSession, s3_bucket_url: str) -> DataFrame:
     try:
-        logger.info(f"Fetching blocks data from {s3_bucket_url}/blocks")
-        blocks_df = spark.read.parquet(f"{s3_bucket_url}/blocks/*", header=True) \
+        logger.info(f"Fetching blocks data from {s3_bucket_url}/blocks...")
+        blocks_df = spark.read.parquet(f"{s3_bucket_url}/blocks/date=2026-*-*", header=True) \
             .select(
+                F.col("date").alias("partition_date"),
                 F.col("number"),
-                F.col("miner"),
+                F.col("hash"),
+                F.col("miner"), 
                 F.col("difficulty"),
                 F.col("total_difficulty"),
                 F.col("size"),
@@ -28,5 +38,28 @@ def extract_blocks_data(spark: SparkSession, s3_bucket_url: str) -> DataFrame:
     except Exception as e:
         logger.error(f"Blocks data extraction failed with unexpected error: {e}")
         raise
+    logger.info("Blocks data was successfully extracted!")
     return blocks_df
 
+def clean_blocks_data(df: DataFrame) -> DataFrame:
+    logger.info(f"Casting column types, trimming stings and formatting dates...")
+    transformed_df = (
+        df
+        .drop_duplicates()
+        .select(
+            F.to_date(F.col("partition_date"), "yyyy-MM-dd"),
+            F.col("number").cast(LongType()),
+            F.trim(F.col("hash").cast(StringType())),
+            F.trim(F.col("miner").cast(StringType())), 
+            F.col("difficulty").cast(DoubleType()),
+            F.col("total_difficulty").cast(DoubleType()),
+            F.col("size").cast(LongType()),
+            F.col("gas_limit").cast(LongType()),
+            F.col("gas_used").cast(LongType()),
+            F.col("base_fee_per_gas").cast(LongType()),
+            F.col("transaction_count").cast(LongType()),
+            F.to_timestamp(F.col("timestamp"))
+        )
+    )
+    logger.info("Blocks data was successfully cleaned!")
+    return transformed_df
