c455df5f173814d982794840f1b4550bff10fe15
Said Osmanov <saidelleausmaneuff@gmail.com>
2026-04-10 21:10:25 +0300

    Add logging to s3

--- etl/utils/spark.py
+++ etl/utils/spark.py
@@ -2,4 +2,9 @@ from pyspark.sql import SparkSession
 
 
 def get_spark_session(app_name: str) -> SparkSession:
-    return SparkSession.builder.appName(app_name).getOrCreate()
+    return (
+        SparkSession.builder.appName(app_name)
+        .config("spark.eventLog.enabled", "true")
+        .config("spark.eventLog.dir", "s3a://ethereum-data/spark-event-logs/")
+        .getOrCreate()
+    )
